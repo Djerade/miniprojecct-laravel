7c18fbcee8984c16426c18f2d746cb96613eac2f
Djeradé Golbé Parfait <djeradegolbeparfait@gmail.com>
2024-01-04 02:46:52 +0000

    passer en parametre une variable

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/', function () {
+//     return "hello world";
+// });
+
 Route::get('/', function () {
-    return view('welcome');
+    $myname = 'perfect';
+    return view('index', compact("myname"));
 });
+
